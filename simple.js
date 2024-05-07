0b2dd0a41b0241c46c481c2baa14832cb69b73a3
FaisalIlyas <faisalilyas2600@gmail.com>
2024-05-08 00:27:48 +0500

    some chaing

--- simple.js
+++ simple.js
@@ -1,12 +1,12 @@
 function Name(){
-    alert("What is your Name?");
-    alert(`Happy Birthday 🎉🎈🥳 🎉🎂 ${prompt()}🎂🎊🥳🎉`);
+    alert("What is Your Name?");
+    alert(`Happy Birthday 🎉🎈🥳 🎂 ${prompt()}🎂🎊`);
 }
 console.log(Name());
 
+
 function SGift(){
 document.write(prompt("Ap ko konsa Gift Chaiye es Birthday pr?"));
-
 
 }
 console.log(SGift());
