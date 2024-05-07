753649d458676ba7c4d23e45005391a1d7a5455d
FaisalIlyas <faisalilyas2600@gmail.com>
2024-05-07 23:51:40 +0500

    Initial commit

---
+++ simple.js
@@ -0,0 +1,13 @@
+function Name(){
+    alert("What is your Name?");
+    alert(`Happy Birthday 🎉🎈🥳 🎉🎂 ${prompt()}🎂🎊🥳🎉`);
+}
+console.log(Name());
+
+function SGift(){
+document.write(prompt("Ap ko konsa Gift Chaiye is Birthday pr?"));
+
+
+}
+console.log(SGift());
+
